4c8d01c1a220b8b20ffabe13d8e6d91a46597608
zry88 <zry88@126.com>
2013-10-31 13:53:37 +0800

    template

---
+++ template/astra/js/g-alert.js
@@ -0,0 +1,1 @@
+(function(e){"use strict";e.fn.gAlert=function(){return this.each(function(){var t=e(this),n=t.find(".g-alert-close");n&&n.click(function(){t.animate({height:"0",margin:0},400,function(){t.css("display","none")})})})}})(jQuery),jQuery(document).ready(function(){"use strict";jQuery(".g-alert").gAlert()});
